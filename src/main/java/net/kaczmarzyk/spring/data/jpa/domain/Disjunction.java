b11058820e9c33822899d8c994e274c800a80d03
Rhys Chang <godjdc@gmail.com>
2020-04-10 16:14:07 +0800

    升版為spring-boot 2.2.6

--- src/main/java/net/kaczmarzyk/spring/data/jpa/domain/Disjunction.java
+++ src/main/java/net/kaczmarzyk/spring/data/jpa/domain/Disjunction.java
@@ -15,16 +15,14 @@
  */
 package net.kaczmarzyk.spring.data.jpa.domain;
 
-import java.util.Arrays;
-import java.util.Collection;
+import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.domain.Specifications;
+import java.util.Arrays;
+import java.util.Collection;
 
 
 /**
@@ -48,10 +46,10 @@ public class Disjunction<T> implements Specification<T> {
     
     @Override
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-        Specifications<T> combinedSpecs = null;
+        Specification<T> combinedSpecs = null;
         for (Specification<T> spec : innerSpecs) {
             if (combinedSpecs == null) {
-                combinedSpecs = Specifications.where(spec);
+                combinedSpecs = Specification.where(spec);
             } else {
                 combinedSpecs = combinedSpecs.or(spec);
             }
